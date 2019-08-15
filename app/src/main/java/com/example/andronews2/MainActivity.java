4fa6eca497e2ecbc7e927cecc4269be8d335c205
Fami1iarStranger <AlexandrGlushko1990@gmail.com>
2019-08-15 23:51:00 +0300

    15.08.2019

--- app/src/main/java/com/example/andronews2/MainActivity.java
+++ app/src/main/java/com/example/andronews2/MainActivity.java
@@ -1,13 +1,9 @@
 package com.example.andronews2;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v4.view.ViewPager;
-import android.view.View;
-import com.example.andronews2.data.Results;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,22 +15,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ViewPager viewPager = findViewById(R.id.viewPager);
-        MyPagerAdapter myPagerAdapter = new MyPagerAdapter(this,getSupportFragmentManager());
+        MyPagerAdapter myPagerAdapter = new MyPagerAdapter(this, getSupportFragmentManager());
         viewPager.setAdapter(myPagerAdapter);
         TabLayout tabLayout = findViewById(R.id.sliding_tabs);
         tabLayout.setupWithViewPager(viewPager);
     }
 
-    public void goToSecondActivity(View v) {
-
-        String a = "";
-        String b = Results.getUrl();
-
-
-        Intent intent = new Intent(this, SecondActivity.class);
-        intent.putExtra("key", a);
-        startActivity(intent);
-
-
-    }
 }
--- app/src/main/java/com/example/andronews2/MyAdapter.java
+++ app/src/main/java/com/example/andronews2/MyAdapter.java
@@ -1,5 +1,6 @@
 package com.example.andronews2;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -28,7 +29,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     }
 
     @Override
-    public void onBindViewHolder(@NotNull MyViewHolder myViewHolder, int position) {
+    public void onBindViewHolder(@NotNull MyViewHolder myViewHolder, final int position) {
         com.example.andronews2.data.Results results = resultsList.get(position);
 
         Glide.with(myViewHolder.img_article.getContext())
@@ -37,6 +38,15 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
         myViewHolder.article.setText(results.getTitle());
         myViewHolder.article_date.setText(results.getPublished_date());
         myViewHolder.section.setText(results.getSection());
+
+        myViewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), SecondActivity.class);
+                intent.putExtra("key", resultsList.get(position).getUrl());
+                v.getContext().startActivity(intent);
+            }
+        });
     }
 
     @Override
